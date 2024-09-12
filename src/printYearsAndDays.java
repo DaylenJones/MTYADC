0dc3f17150dc4207afa55b17ce99f7640f367bce
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-12 13:31:49 -0400

    update

--- src/printYearsAndDays.java
+++ src/printYearsAndDays.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class printYearsAndDays {
+public class Main {
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+
+    }
+
+    private static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long years = days / 365;
+            long leftoverDays = days % 365;
+
+            system.out.println(minutes + " min = " + years + "y and" + leftoverDays + "d");
+        }
+
+
+    }
 }
